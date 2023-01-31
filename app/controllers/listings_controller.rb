47ee6b4d1218ffd3feb45a43878022b18a4a0898
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2023-01-31 22:22:32 +0100

    Go

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,15 +6,14 @@ class ListingsController < ApplicationController
 
   def new
     @listing = Lintings.new(listing_params)
+    
     if @listing.save
       redirect_to index
     end
   end
 
-
-
   private
  def listing_params
-   params.require([:listings ]).permit()
+  params.require([:listings ]).permit()
  end
 end
