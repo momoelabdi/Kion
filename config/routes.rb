f048338fbeb4eed7f96e53074d34b5888bb90cc7
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2023-02-01 17:33:19 +0100

    htmlize

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources  :listings, only: [:new, :create, :show, :edit, :update, :destroy]
+
+resources :listings, only: [:new, :create, :show, :edit, :update, :destroy]
 
 root to: 'listings#index'
 get '/listings/new', to: 'listings#new'
 post '/listings', to: 'listings#create'
+get '/listings/:id', to: 'listings#show'
 end
